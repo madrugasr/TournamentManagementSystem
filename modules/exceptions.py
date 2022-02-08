3d6f5eb9b6370f2a84a27df6dbdf790b591dd9e7
Igor Ferreira <igor-freitas10@hotmail.com>
2022-02-08 10:45:18 -0300

    Corrige importações em exceptions.py

--- modules/exceptions.py
+++ modules/exceptions.py
@@ -1,28 +1,47 @@
-from os import close, name, system
-from main import menu
+import sys
+from os import name, system
 
-def linha(vezes): """Função: Linha""", print('\033[1;33m==\033[m' * vezes)
+
+def linha(vezes):
+    """
+    Função: Linha
+    """
+    print('\033[1;33m==\033[m' * vezes)
+
 
 def limpa_tela_menu():
     """
     Função: Limpar a tela e retorna a função Menu.
     """
-    if name == 'nt': system('cls'), menu()
-    else: system('clear'), menu()
+    if name == 'nt':
+        system('cls')
+    else:
+        system('clear')
+
 
 def limpa_tela():
     """
     Função: Limpar a Tela.
     """
-    if name == 'nt': system('cls')
-    else: system('clear')
+    if name == 'nt':
+        system('cls')
+    else:
+        system('clear')
 
-def sair(): """Função: Sair""", limpa_tela(), close()
+
+def sair():
+    """
+    Função: Sair
+    """
+    limpa_tela()
+    sys.exit()
+
 
 def guardar_ficheiro():
     """
     Função: Salvar Ficheiro.
     """
+
 
 def carregar_ficheiro():
     """
